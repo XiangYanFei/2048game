41efa5e4400da0732becbd7a7a249335f4955f48
XiangYanFei <470689887@qq.com>
2017-06-10 16:03:50 +0800

    重构+添加键盘监听事件

--- index.js
+++ index.js
@@ -24,23 +24,45 @@ for (var i = 0; i < 4; i++) {
         app.stage.addChild(graphics);
     }
 }
-var x = generateNumberRandom();
-var y = generateNumberRandom();
-
-var graphics = new PIXI.Graphics();
-graphics.beginFill(0xFFC125, 1);
-graphics.drawRect(app.renderer.width / 8 + x * 73, app.renderer.height / 8 * 3 + y * 73, 70, 70);
-app.stage.addChild(graphics);
-
-var number = new PIXI.Text('2', {
-    fontSize: 60,
-    fill: 'white'
-});
-number.anchor.set(0.5);
-number.x = 35 + app.renderer.width / 8 + x * 73;
-number.y = 35 + app.renderer.height / 8 * 3 + y * 73;
-app.stage.addChild(number);
 
 function generateNumberRandom() {
     return Math.floor(Math.random() * 4);
 }
+
+function drawCell(rowIndex, columnIndex) {
+    var color = 0XFFEC8B;
+
+    if (grid[rowIndex][columnIndex] == 2) {
+        color = 0xFFC125;
+    }
+
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(color, 1);
+    graphics.drawRect(app.renderer.width / 8 + columnIndex * 73, app.renderer.height / 8 * 3 + rowIndex * 73, 70, 70);
+    app.stage.addChild(graphics);
+
+    if (grid[rowIndex][columnIndex] != 0) {
+        var number = new PIXI.Text(grid[rowIndex][columnIndex], {
+            fontSize: 60,
+            fill: 'white'
+        });
+        number.anchor.set(0.5);
+        number.x = 35 + app.renderer.width / 8 + columnIndex * 73;
+        number.y = 35 + app.renderer.height / 8 * 3 + rowIndex * 73;
+        app.stage.addChild(number);
+    }
+}
+
+var rowIndex = generateNumberRandom();
+var columnIndex = generateNumberRandom();
+
+grid[rowIndex][columnIndex] = 2;
+drawCell(rowIndex, columnIndex);
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'ArrowRight') {
+        console.log(event);
+   }
+});
+
+
